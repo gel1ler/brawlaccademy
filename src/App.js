6e73f55a098c13e9f4dda47dd8c4d71ae462211d
gel1ler <57824633+gel1ler@users.noreply.github.com>
2021-09-07 15:35:33 +0300

    -carousel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import Header from './components/header'
 import Content from './components/content/content'
-import { createMuiTheme } from '@material-ui/core/styles';
 import './style.css'
 import TopSlide from './UI/topSlide'
-import Carousel from './UI/carousel/carousel'
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
       <Content />
-      <Carousel />
       <TopSlide />
     </div>
   )
